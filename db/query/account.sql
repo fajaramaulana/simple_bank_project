eb61ac6232be0eaf410a44332222e6b47fab49d0
fajaramaulana <fajaragusmaulana@gmail.com>
2024-06-23 14:53:48 +0700

    Feat: defile .sql query for account and  sqlc generates go code for account model based on predefined queries

---
+++ db/query/account.sql
@@ -0,0 +1,36 @@
+-- name: CreateAccount :one
+INSERT INTO accounts (
+  owner,
+  balance,
+  currency
+) VALUES (
+  $1, $2, $3
+) RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE deleted_at IS NULL AND id = $1 LIMIT 1;
+
+-- name: GetAccountForUpdate :one
+SELECT * FROM accounts
+WHERE deleted_at IS NULL AND id = $1 LIMIT 1
+FOR NO KEY UPDATE;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+WHERE deleted_at IS NULL AND  owner = $1
+ORDER BY id
+LIMIT $2
+OFFSET $3;
+
+-- name: UpdateAccount :one
+UPDATE accounts
+SET balance = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: SoftDeleteAccount :exec
+UPDATE accounts
+SET deleted_at = now()
+WHERE id = $1
+RETURNING *;
