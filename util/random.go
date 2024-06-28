6a01c39b11f5641c1e641f9d3a48ee22abd97bf4
fajaramaulana <fajaragusmaulana@gmail.com>
2024-06-28 23:27:27 +0700

    Feat: make base for generate random

--- util/random.go
+++ util/random.go
@@ -2,9 +2,17 @@ package util
 
 import (
 	"errors"
+	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/go-faker/faker/v4"
 )
+
+func NewRandomMoneyGenerator() *rand.Rand {
+	seed := rand.NewSource(time.Now().UnixNano())
+	return rand.New(seed)
+}
 
 // Random Int
 func RandomInt(n int) ([]int, error) {
@@ -24,4 +32,9 @@ func RandomName() string {
 
 func RandomCurrency() string {
 	return faker.Currency()
+}
+
+func RandomMoney(r *rand.Rand, min, max float64) string {
+	amount := min + r.Float64()*(max-min)
+	return fmt.Sprintf("%.2f", amount)
 }
