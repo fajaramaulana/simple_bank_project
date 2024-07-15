42520a841f72daabd378e6900ae282924043eaaa
fajaramaulana <fajaragusmaulana@gmail.com>
2024-07-15 16:09:37 +0700

    fix: Fix deadlock issue in transfer transaction

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -33,6 +33,7 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 		if rbErr := tx.Rollback(); rbErr != nil {
 			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
 		}
+		return err
 	}
 
 	return tx.Commit()
@@ -89,20 +90,7 @@ func (store *Store) TransferTx(ctx context.Context, param TransferTxParam) (Tran
 		}
 
 		// TODO update account balance
-		result.FromAccount, err = q.SubtractAccountBalance(ctx, SubtractAccountBalanceParams{
-			Amount: strconv.FormatInt(param.Amount, 10),
-			ID:     param.FromAccountID,
-		})
-
-		if err != nil {
-			return err
-		}
-
-		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			Amount: strconv.FormatInt(param.Amount, 10),
-			ID:     param.ToAccountID,
-		})
-
+		result.FromAccount, result.ToAccount, err = addBalance(ctx, q, param.FromAccountID, param.Amount, param.ToAccountID, param.Amount)
 		if err != nil {
 			return err
 		}
@@ -111,4 +99,48 @@ func (store *Store) TransferTx(ctx context.Context, param TransferTxParam) (Tran
 	})
 
 	return result, err
+}
+
+func addBalance(ctx context.Context, q *Queries, accountID1, amount1, accountID2, amount2 int64) (fromAccount Account, toAccount Account, err error) {
+	if accountID1 < accountID2 {
+		fromAccount, err := q.SubtractAccountBalance(ctx, SubtractAccountBalanceParams{
+			Amount: strconv.FormatInt(amount1, 10),
+			ID:     accountID1,
+		})
+
+		if err != nil {
+			return fromAccount, toAccount, err
+		}
+
+		toAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			Amount: strconv.FormatInt(amount2, 10),
+			ID:     accountID2,
+		})
+
+		if err != nil {
+			return fromAccount, toAccount, err
+		}
+
+		return fromAccount, toAccount, nil
+	} else {
+		toAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			Amount: strconv.FormatInt(amount2, 10),
+			ID:     accountID2,
+		})
+
+		if err != nil {
+			return fromAccount, toAccount, err
+		}
+
+		fromAccount, err := q.SubtractAccountBalance(ctx, SubtractAccountBalanceParams{
+			Amount: strconv.FormatInt(amount1, 10),
+			ID:     accountID1,
+		})
+
+		if err != nil {
+			return fromAccount, toAccount, err
+		}
+
+		return fromAccount, toAccount, nil
+	}
 }
--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -157,6 +157,74 @@ func TestTransTx(t *testing.T) {
 	require.Equal(t, accountTwoBalance+float64(n)*float64(amount), updatedAccount2Balance)
 }
 
+func TestTransTxDeadlock(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := generateAccount(t)
+	account2 := generateAccount(t)
+
+	fmt.Println(">> before:", account1.Balance, account2.Balance)
+
+	// make random ["credit", "debit"]
+	transactionType := getRandomOption([]string{"credit", "debit"})
+
+	// run n concurent transfer transaction
+	n := 10
+	amount := int64(10)
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+		if i%2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+		go func() {
+			_, err := store.TransferTx(context.Background(), TransferTxParam{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+				Type:          transactionType,
+			})
+
+			errs <- err
+
+		}()
+	}
+
+	// check results
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+	}
+
+	// check the final updated balance
+	updatedAccount1, err := store.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := store.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+	accountOneBalance, err := strconv.ParseFloat(account1.Balance, 64)
+	require.NoError(t, err)
+	accountTwoBalance, err := strconv.ParseFloat(account2.Balance, 64)
+	require.NoError(t, err)
+
+	// convert updatedAccount1.Balance string to float64
+	updatedAccount1Balance, err := strconv.ParseFloat(updatedAccount1.Balance, 64)
+	require.NoError(t, err)
+
+	// convert updatedAccount2.Balance string to float64
+	updatedAccount2Balance, err := strconv.ParseFloat(updatedAccount2.Balance, 64)
+	require.NoError(t, err)
+
+	require.Equal(t, accountOneBalance, updatedAccount1Balance)
+	require.Equal(t, accountTwoBalance, updatedAccount2Balance)
+}
+
 func getRandomOption(options []string) string {
 	n := len(options)
 	idx := rand.Intn(n)
