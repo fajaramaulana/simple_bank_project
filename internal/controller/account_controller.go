d97c6847aa47495d6e78364483e8ad59be289f7a
fajaramaulana <fajaragusmaulana@gmail.com>
2024-07-17 19:37:18 +0700

    chore: Refactor account_controller.go for improved code organization and readability

--- internal/controller/account_controller.go
+++ internal/controller/account_controller.go
@@ -11,16 +11,27 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// AccountController handles HTTP requests related to accounts.
 type AccountController struct {
 	accountService *service.AccountService
 }
 
+// NewAccountController creates a new instance of the AccountController struct.
+// It takes an accountService parameter of type *service.AccountService and returns a pointer to the AccountController.
 func NewAccountController(accountService *service.AccountService) *AccountController {
 	return &AccountController{
 		accountService: accountService,
 	}
 }
 
+// CreateAccount creates a new account based on the provided JSON request.
+// It first binds the JSON request to the CreateAccountRequest struct.
+// If there is an error in binding the JSON, it returns a JSON error response with the error message and data.
+// Next, it performs validation on the request data. If there are validation errors, it returns a JSON error response with the validation errors.
+// If the request data is valid, it calls the accountService's CreateAccount method to create the account.
+// If there is an error in creating the account, it returns a JSON error response with the error message.
+// If the account already exists, it returns a JSON error response indicating that the account already exists.
+// If the account is created successfully, it returns a JSON response with the status code 201 and the created account.
 func (a *AccountController) CreateAccount(ctx *gin.Context) {
 	var request request.CreateAccountRequest
 	if err := ctx.ShouldBindJSON(&request); err != nil {
@@ -55,6 +66,10 @@ func (a *AccountController) CreateAccount(ctx *gin.Context) {
 	helper.ReturnJSON(ctx, http.StatusCreated, "Account created", account)
 }
 
+// GetAccount retrieves an account based on the provided UUID.
+// It expects the UUID to be passed as a parameter in the request URI.
+// If the UUID is invalid or the account is not found, appropriate error responses are returned.
+// If the account is found, it is returned as a JSON response with a success status code.
 func (a *AccountController) GetAccount(ctx *gin.Context) {
 	// uuid := ctx.Param("uuid")
 	var req request.GetAccountRequest
@@ -91,6 +106,21 @@ func (a *AccountController) GetAccount(ctx *gin.Context) {
 	helper.ReturnJSON(ctx, http.StatusOK, "Account found", account)
 }
 
+// GetAccounts retrieves a list of accounts based on the provided query parameters.
+// It binds the query parameters from the request context, validates them, and then
+// calls the accountService to fetch the accounts from the database. The function
+// returns the list of accounts along with the total number of data, or an error
+// if there was a problem with the request or the database.
+//
+// Parameters:
+// - ctx: The gin.Context object representing the HTTP request and response.
+//
+// Returns:
+// - None
+//
+// Example usage:
+//
+//	router.GET("/accounts", accountController.GetAccounts)
 func (a *AccountController) GetAccounts(ctx *gin.Context) {
 	var req request.ListAccountRequest
 
@@ -123,6 +153,16 @@ func (a *AccountController) GetAccounts(ctx *gin.Context) {
 	helper.ReturnJSONWithMetaPage(ctx, http.StatusOK, "Account found", accounts, int(totalData), len(accounts), int(req.Page), int(req.Limit))
 }
 
+// UpdateAccount updates the account information based on the provided request.
+// It first binds the URI parameters and checks for any binding errors.
+// If there are binding errors, it returns a JSON error response with the error details.
+// Then, it converts the UUID string to a UUID type and checks for any conversion errors.
+// If there are conversion errors, it returns a JSON error response with the error details.
+// Next, it binds the JSON request body and checks for any binding errors.
+// If there are binding errors, it returns a JSON error response with the error details.
+// After that, it performs validation on the request data and checks for any validation errors.
+// If there are validation errors, it returns a JSON error response with the error details.
+// Finally, it updates the account in the database and returns a JSON response with the updated account information.
 func (a *AccountController) UpdateAccount(ctx *gin.Context) {
 	var req request.GetAccountRequest
 
