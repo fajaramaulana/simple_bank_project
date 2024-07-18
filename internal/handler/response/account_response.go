34b6c18d6853d98ef4c0612c27202efdb97fdec0
fajaramaulana <fajaragusmaulana@gmail.com>
2024-07-18 12:33:50 +0700

    chore: Update account response structs to include user information

--- internal/handler/response/account_response.go
+++ internal/handler/response/account_response.go
@@ -7,19 +7,19 @@ import (
 )
 
 type AccountResponseCreate struct {
-	AccountUUID uuid.UUID `json:"account_uuid"`
-	Owner       string    `json:"owner"`
-	Email       string    `json:"email"`
-	Currency    string    `json:"currency"`
-	Balance     string    `json:"balance"`
+	AccountUUID uuid.UUID     `json:"account_uuid"`
+	Owner       string        `json:"owner"`
+	Currency    string        `json:"currency"`
+	Balance     string        `json:"balance"`
+	User        UserGetSimple `json:"user"`
 }
 
 type AccountResponseGet struct {
-	AccountUUID uuid.UUID `json:"account_uuid"`
-	Owner       string    `json:"owner"`
-	Email       string    `json:"email"`
-	Currency    string    `json:"currency"`
-	Balance     string    `json:"balance"`
-	CreatedAt   time.Time `json:"created_at"`
-	Status      int32     `json:"status"`
+	AccountUUID uuid.UUID     `json:"account_uuid"`
+	Owner       string        `json:"owner"`
+	Currency    string        `json:"currency"`
+	Balance     string        `json:"balance"`
+	CreatedAt   time.Time     `json:"created_at"`
+	Status      int32         `json:"status"`
+	User        UserGetSimple `json:"user"`
 }
