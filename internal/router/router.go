94657ba7cd93e8b113935ca0076426addada1129
fajaramaulana <fajaragusmaulana@gmail.com>
2024-07-22 22:21:30 +0700

    refactor: Add middleware for authentication in router.go

--- internal/router/router.go
+++ internal/router/router.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/fajaramaulana/simple_bank_project/internal/controller"
 	"github.com/fajaramaulana/simple_bank_project/internal/handler/helper"
+	"github.com/fajaramaulana/simple_bank_project/internal/handler/middleware"
 	"github.com/fajaramaulana/simple_bank_project/internal/handler/token"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
@@ -20,6 +21,10 @@ type Router struct {
 	TokenMaker  token.Maker
 }
 
+// NewRouter creates a new instance of the Router struct and initializes its dependencies.
+// It takes an AccountController, TransactionController, UserController, AuthController,
+// and a map of configuration tokens as parameters. It returns a pointer to the Router
+// struct and an error if there was an issue creating the token maker.
 func NewRouter(account *controller.AccountController, transaction *controller.TransactionController, user *controller.UserController, auth *controller.AuthController, configToken map[string]string) (*Router, error) {
 	tokenMaker, err := token.NewPasetoMaker(configToken["token_secret"])
 	if err != nil {
@@ -55,20 +60,22 @@ func (r *Router) SetupRouter() {
 		})
 	})
 
-	// account
-	v1.POST("/account", r.account.CreateAccount)
-	v1.GET("/account/:uuid", r.account.GetAccount)
-	v1.GET("/accounts", r.account.GetAccounts)
-	v1.PUT("/account/:uuid", r.account.UpdateAccount)
-
-	// transaction
-	v1.POST("/transaction", r.transaction.CreateTransfer)
-
-	// user
-	v1.POST("/user", r.user.CreateUser)
-
 	// auth
 	v1.POST("/auth/login", r.auth.Login)
+
+	authRoutesV1 := router.Group("/api/v1").Use(middleware.AuthMiddleware(r.TokenMaker))
+
+	// account
+	authRoutesV1.POST("/account", r.account.CreateAccount)
+	authRoutesV1.GET("/account/:uuid", r.account.GetAccount)
+	authRoutesV1.GET("/accounts", r.account.GetAccounts)
+	authRoutesV1.PUT("/account/:uuid", r.account.UpdateAccount)
+
+	// transaction
+	authRoutesV1.POST("/transaction", r.transaction.CreateTransfer)
+
+	// user
+	authRoutesV1.POST("/user", r.user.CreateUser)
 
 	r.Engine = router
 }
