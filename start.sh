3d21cfbe30e59ec8c2cfc8db96b828c78ececf5f
fajaramaulana <fajaragusmaulana@gmail.com>
2024-07-24 23:13:15 +0700

    refactor: Fix typo in start.sh script

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "run db migration"
-echo "postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME?sslmode=$DB_SSLMODE"
+soucre /app/app.env
 /app/migrate -path /app/migration -database "postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME?sslmode=$DB_SSLMODE" -verbose up
 
 echo "start app"
